ac4997251724ab0c099210416dcba96892550c39
vikram u k <vikramuk@gmail.com>
2018-12-24 10:40:47 +0530

    Check PID Response

---
+++ CheckPID.java
@@ -0,0 +1,3 @@
+int[] expected = new int[]{Integer.parseInt("10111110", 2), Integer.parseInt("00011111", 2),
+               Integer.parseInt("10101000", 2), Integer.parseInt("00010011", 2)};
+System.out.println(expected[0],expected[1],expected[2],expected[3]);
